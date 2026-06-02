682a49954c759e932e7c08a8fd0edf141d236e6a
Felipe Queiroz <mitaovlog00@gmail.com>
2026-06-02 19:54:38 -0300

    Use clean internal URLs

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@ const stackFilters = document.querySelectorAll('.stack-filter');
 const stackItems = document.querySelectorAll('.stack-cloud span');
 const tiltTargets = document.querySelectorAll('[data-tilt]');
 
+const normalizeInternalPath = (path) => path
+  .replace(/\/(?:inde|index)\.xhtml$/i, '/')
+  .replace(/\/index\.html$/i, '/')
+  .replace(/\/([a-z0-9-]+)\.html$/i, '/$1');
+
+const normalizedPath = normalizeInternalPath(window.location.pathname);
+
+if (normalizedPath !== window.location.pathname) {
+  window.history.replaceState(null, '', `${normalizedPath}${window.location.search}${window.location.hash}`);
+}
+
+document.querySelectorAll('a[href]').forEach((link) => {
+  const normalizedHref = link.getAttribute('href')
+    .replace(/^(?:inde|index)\.xhtml$/i, './')
+    .replace(/^index\.html$/i, './')
+    .replace(/^([a-z0-9-]+)\.html$/i, '$1');
+
+  link.setAttribute('href', normalizedHref);
+});
+
 consoleTabs.forEach((tab) => {
   tab.addEventListener('click', () => {
     const panelName = tab.dataset.panel;
